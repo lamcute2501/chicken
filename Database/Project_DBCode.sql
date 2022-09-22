86a36ec22aa18e1a709db300a68a6abf3309335d
lamcute2501 <nguyenrung1203@gmail.com>
2022-09-22 14:30:14 +0700

    update

--- Database/Project_DBCode.sql
+++ Database/Project_DBCode.sql
@@ -14,17 +14,16 @@ create table chicken(
 create table chickenstatus(
 	chicken_id int not null,
     quantity int not null default 0,
-    chicken_status Enum('Giống','Đang phát triển','Xuất chuồng') not null,
-    constraint pk_chickenstatus primary key (chicken_id)
-    -- constraint fk_chickenstatus_chicken foreign key (chicken_id) references chicken(chicken_id)
+    chicken_status Enum('Giống','Nhỡ','Xuất Chuồng') not null,
+    cage_id int not null
 );
 
 create table cage(
 	cage_id int primary key auto_increment,
     cage_name varchar(50) not null,
-    max_capacity int not null default 0,
     current_capacity int not null default 0,
-    cage_status enum('Đang hoạt động','Ngừng hoạt động','Bảo trì') not null
+    max_capacity int not null default 0,
+    cage_status enum('Hoạt Động','Đóng','Bảo Trì') not null
 );
 
 create table transaction(
@@ -35,16 +34,22 @@ create table transaction(
     transaction_time datetime default now() not null
 );
 
+create table user(
+	user_id int primary key auto_increment,
+    user_name varchar(50) unique not null,
+    user_password varchar(50) not null
+);
 
 -- alter table chicken auto_increment = 1;
 -- creat procedure,trigger,view
 
 -- chicken start --------------------------------------------
 DELIMITER $$
-CREATE PROCEDURE `AddChicken`(in name varchar(50),in im_price decimal(20,2), in ex_price decimal(20,2), in description varchar(200))
+CREATE PROCEDURE `AddChicken`(in name varchar(50),in im_price decimal(20,2), in ex_price decimal(20,2), in description varchar(200), out id int)
 BEGIN
 	insert into chicken(chicken_name,import_price,export_price,description) values
     (name,im_price,ex_price,description);
+    select max(chicken_id) into id from chicken;
 END$$
 DELIMITER ;
 
@@ -56,15 +61,35 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE `UpdateChickenInfo`(in name varchar(50),in im_price decimal(20,2), in ex_price decimal(20,2), in description varchar(200),in id int)
+CREATE PROCEDURE `UpdateChickenInfo`(in name varchar(50),in im_price decimal(20,2), in ex_price decimal(20,2), in des varchar(200),in id int)
 BEGIN
 	update chicken
-    set chicken_name = name,import_price = im_price, export_price = ex_price, description = description
+    set chicken_name = name,import_price = im_price, export_price = ex_price, description = des
     where chicken_id = id;
 END$$
 DELIMITER ;
 
--- drop trigger before_chicken_insert;
+DELIMITER $$
+CREATE PROCEDURE `SearchChickenByID`(in id int)
+BEGIN
+	select * from chicken where chicken_id  = id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `SearchChickenByName`(in name varchar(50))
+BEGIN
+	select * from chicken where chicken_name  = name;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `GetAllChicken`()
+BEGIN
+	select * from chicken;
+END$$
+DELIMITER ;
+
 delimiter $$
 create trigger before_chicken_insert before insert 
 on chicken for each row
@@ -73,7 +98,7 @@ begin
     end if;
     if(new.export_price < 0) then signal sqlstate '45000' set message_text = 'tg_before_insert: export price must >= 0';
     end if;
-    if((select count(new.chicken_name) from chicken) = 1) then signal sqlstate '45000' set message_text = 'tg_before_insert: chicken already exists';
+    if((select count(chicken_id) from chicken where chicken_name = new.chicken_name) = 1) then signal sqlstate '45000' set message_text = 'tg_before_insert: chicken already exists';
     end if;
 end $$
 delimiter ;
@@ -86,20 +111,18 @@ begin
     end if;
     if(new.export_price < 0) then signal sqlstate '45000' set message_text = 'tg_before_update: export price must >= 0';
     end if;
-    if((select count(new.chicken_name) from chicken) = 1) then signal sqlstate '45000' set message_text = 'tg_before_update: chicken already exists';
+    if((select count(chicken_id) from chicken where chicken_name = new.chicken_name and chicken_name <> old.chicken_name) = 1) then signal sqlstate '45000' set message_text = 'tg_before_update: chicken already exists';
     end if;
 end $$
 delimiter ;
 
-
-
 -- chicken status start --------------------------------------------
 
 DELIMITER $$
-CREATE PROCEDURE `AddStatus`(in id int, in quantity int, in status enum('Giong','Dang Lon','Xuat Chuong'))
+CREATE PROCEDURE `AddStatus`(in id int, in qtity int, in status enum('Giống','Nhỡ','Xuất Chuồng'), in cage int)
 BEGIN
-	insert into chickenstatus(chicken_id,quantity,chicken_status) values
-    (id,quantity,status);
+	insert into chickenstatus(chicken_id,quantity,chicken_status,cage_id) values
+    (id,qtity,status,cage);
 END$$
 DELIMITER ;
 
@@ -111,11 +134,41 @@ DELIMITER ;
 -- DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE `UpdateChickenStatus`(in id int, in quantity int, in status Enum('Giong','Dang Lon','Xuat Chuong'))
+CREATE PROCEDURE `SearchChickenStatusByID`(in id int)
+BEGIN
+	select * from chickenstatus where chicken_id  = id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `SearchChickenStatusByStatus`(in status Enum('Giống','Nhỡ','Xuất Chuồng'))
+BEGIN
+	select * from chickenstatus where chicken_status  = status;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE `SearchChickenStatus`(in chicken int,in cage int, in status Enum('Giống','Nhỡ','Xuất Chuồng'))
+BEGIN
+	select * from chickenstatus where chicken_status  = status and chicken_id = chicken and cage_id = cage;
+END$$
+DELIMITER ;
+
+-- khi chuyen chuong kich hoat trigger tru cua chuong nay + vao chuong kia 
+DELIMITER $$
+CREATE PROCEDURE `UpdateChickenStatus`(in id int, in qtity int, in status Enum('Giống','Nhỡ','Xuất Chuồng'), in oldCage int, in newcage int)
 BEGIN
 	update chickenstatus
-    set quantity = quantity
-    where chicken_id = id and chicken_status = status;
+    set quantity = qtity, cage_id = newCage
+    where chicken_id = id and chicken_status = status and cage_id = oldCage;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `GetAllChickenStatus`()
+BEGIN
+	select * from chickenstatus;
 END$$
 DELIMITER ;
 
@@ -123,9 +176,9 @@ delimiter $$
 create trigger before_chickenstatus_insert before insert 
 on chickenstatus for each row
 begin
-	if(new.quantity <= 0) then signal sqlstate '45000' set message_text = 'tg_before_insert: quantity must > 0';
+	if(new.quantity < 0) then signal sqlstate '45000' set message_text = 'tg_before_insert: quantity must >= 0';
     end if;
-    if((select count(quantity) from chickenstatus where chicken_id = new.chicken_id and chicken_status = new.chicken_status) = 1)
+    if((select count(quantity) from chickenstatus where chicken_id = new.chicken_id and chicken_status = new.chicken_status and cage_id = new.cage_id) = 1)
     then signal sqlstate '45000' set message_text = 'tg_before_insert: chicken status already exists';
     end if;
 end $$
@@ -135,7 +188,7 @@ delimiter $$
 create trigger before_chickenstatus_update before update
 on chickenstatus for each row
 begin
-	if(new.quantity <= 0) then signal sqlstate '45000' set message_text = 'tg_before_update: quantity must > 0';
+	if(new.quantity < 0) then signal sqlstate '45000' set message_text = 'tg_before_update: quantity must >= 0';
     end if;
 end $$
 delimiter ;
@@ -144,10 +197,11 @@ delimiter ;
 -- cage start --------------------------------------------
 
 DELIMITER $$
-CREATE PROCEDURE `AddCage`(in name varchar(50), in max_cap int, in cur_cap int ,in status enum('Open','Close','Maintenance'))
+CREATE PROCEDURE `AddCage`(in name varchar(50), in max_cap int, in cur_cap int ,in status enum('Đóng','Hoạt Động','Bảo Trì'), out id int)
 BEGIN
 	insert into cage(cage_name,max_capacity,current_capacity,cage_status) values
     (name,max_cap,cur_cap,status);
+    select max(cage_id) into id from cage;
 END$$
 DELIMITER ;
 
@@ -159,7 +213,21 @@ DELIMITER ;
 -- DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE `UpdateCage`(in id int, in name varchar(50), in max_cap int, in cur_cap int ,in status Enum('Open','Close','Maintenance'))
+CREATE PROCEDURE `SearchCageByID`(in id int)
+BEGIN
+	select * from cage where cage_id  = id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `SearchCageByName`(in name varchar(50))
+BEGIN
+	select * from cage where cage_name  = name;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `UpdateCage`(in id int, in name varchar(50), in max_cap int, in cur_cap int ,in status Enum('Đóng','Hoạt Động','Bảo Trì'))
 BEGIN
 	update cage
     set cage_name = name,max_capacity = max_cap,current_capacity = cur_cap,cage_status = status
@@ -167,8 +235,15 @@ BEGIN
 END$$
 DELIMITER ;
 
--- mot chuong dong hoac bao tri se khong the chua ga, khong the update voi trang thai close hoac maintenance voi so ga nhieu hon 0
+DELIMITER $$
+CREATE PROCEDURE `GetAllCage`()
+BEGIN
+	select * from cage;
+END$$
+DELIMITER ;
 
+-- mot chuong dong hoac bao tri se khong the chua ga, khong the update voi trang thai close hoac maintenance voi so ga nhieu hon 0
+-- mot chuong so luong hien tai khong bi vuot qua
 delimiter $$
 create trigger before_cage_insert before insert 
 on cage for each row
@@ -177,7 +252,10 @@ begin
     end if;
     if(new.current_capacity < 0) then signal sqlstate '45000' set message_text = 'tg_before_insert: current capacity must >= 0';
     end if;
-    if(new.current_capacity > 0 and (new.cage_status = 'Close' or new.cage_status = 'Maintenance')) 
+    -- herreeeeeeeee
+    if(new.current_capacity > new.max_capacity) then signal sqlstate '45000' set message_text = 'tg_before_insert: current capacity cannot greater than max capacity';
+    end if;
+    if(new.current_capacity > 0 and (new.cage_status = 'Đóng' or new.cage_status = 'Bảo Trì')) 
     then signal sqlstate '45000' set message_text = 'tg_before_insert: quantity and status are not compatible';
     end if;
     if((select count(cage_name) from cage where cage_name = new.cage_name) = 1)
@@ -194,9 +272,12 @@ begin
     end if;
     if(new.current_capacity < 0) then signal sqlstate '45000' set message_text = 'tg_before_update: current capacity must >= 0';
     end if;
-    if(new.current_capacity > 0 and (new.cage_status = 'Close' or new.cage_status = 'Maintenance')) 
+    if(new.current_capacity > new.max_capacity) then signal sqlstate '45000' set message_text = 'tg_before_update: current capacity cannot greater than max capacity';
+    end if;
+    if(new.current_capacity > 0 and (new.cage_status = 'Đóng' or new.cage_status = 'Bảo Trì')) 
     then signal sqlstate '45000' set message_text = 'tg_before_update: quantity and status are not compatible';
     end if;
+    
 end $$
 delimiter ;
 
@@ -204,3 +285,13 @@ delimiter ;
 -- transaction start --------------------------------------------
 
 
+-- user start ----------------------------------------------
+
+DELIMITER $$
+CREATE PROCEDURE `SearchUserName`(in userName varchar(50), out id int)
+BEGIN
+	select user_id from user where user_name = userName;
+END$$
+DELIMITER ;
+
+ select user_id from user where user_name = 'hakira' and user_password = '123123123';
